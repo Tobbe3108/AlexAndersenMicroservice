6ed8f97b0a71b9bbabccc0b55ead700be50cb7d8
Frederik Hegnet Petersen <frederikhegnetpetersen@gmail.com>
2021-10-27 14:11:16 +0200

    Add integration tests to delivery microservice

---
+++ src/Delivery.Microservice/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Delivery.Microservice.Interfaces;
+using Delivery.Microservice.Services;
+
+namespace Delivery.Microservice;
+
+public class Startup
+{
+  public Startup(IConfiguration configuration)
+  {
+    Configuration = configuration;
+  }
+
+  public IConfiguration Configuration { get; }
+
+  public void ConfigureServices(IServiceCollection services)
+  {
+    services.AddTransient<IDeliveryService, DeliveryService>();
+    services.AddTransient<ISqlService, SqlService>();
+
+    services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+  }
+
+  public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+  {
+    if (env.IsDevelopment())
+    {
+      app.UseDeveloperExceptionPage();
+      app.UseSwagger();
+      app.UseSwaggerUI();
+    }
+    else
+    {
+      app.UseExceptionHandler("/Error");
+      app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.UseEndpoints(endpoints =>
+    {
+      endpoints.MapControllers();
+    });
+  }
+}
