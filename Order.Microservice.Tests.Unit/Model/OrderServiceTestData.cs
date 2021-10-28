b3aa75f327605fbb09343a517c90ff8f1c2b2c9a
simonhanseen <47739651+simonhanseen@users.noreply.github.com>
2021-10-28 09:47:04 +0200

    Adding Theory for both static IEnumerable and Class with IEnumerable TestData

---
+++ Order.Microservice.Tests.Unit/Model/OrderServiceTestData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Microservice.Tests.Unit.Model
+{
+    class OrderServiceTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 1, DateTime.Now, 1, "Updated", "1" };
+            yield return new object[] { 1, DateTime.Now, "IdAsString", "Updated", "1" };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
--- Order.Microservice.Tests.Unit/OrderServiceTests.cs
+++ Order.Microservice.Tests.Unit/OrderServiceTests.cs
@@ -6,6 +6,8 @@ using System;
 using System.Threading.Tasks;
 using Xunit;
 using FluentAssertions;
+using System.Collections.Generic;
+using Order.Microservice.Tests.Unit.Model;
 
 namespace Order.Microservice.Tests.Unit
 {
@@ -20,11 +22,12 @@ namespace Order.Microservice.Tests.Unit
         }
 
 
-        [Fact]
-        public async Task CreateOrder_ShouldReturnCreateOrder()
+        [Theory]
+        [ClassData(typeof(OrderServiceTestData))]
+        public async Task CreateOrder_ShouldReturnCreateOrder(int orderId, DateTime createdDate, int customerId, string deliveryAddress, string orderNo)
         {
             // Arrange
-            var orderToCreate = new Models.Order { OrderId = 1, CreatedDate = DateTime.Now, CustomerId = 1, DeliveryAddress = "Boulevard", OrderNo = "1" };
+            var orderToCreate = new Models.Order { OrderId = orderId, CreatedDate = createdDate, CustomerId = customerId, DeliveryAddress = deliveryAddress, OrderNo = orderNo };
 
             _sqlService.Setup(service => service.CreateOrder(orderToCreate))
                 .ReturnsAsync(orderToCreate);
@@ -54,14 +57,15 @@ namespace Order.Microservice.Tests.Unit
             actual?.OrderId.Should().Be(orderId);
         }
 
-        [Fact]
-        public async Task UpdateOrder_ShouldReturnUpdatedDeliveryAddress()
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public async Task UpdateOrder_ShouldReturnUpdatedDeliveryAddress(int orderId, DateTime createdDate, int customerId, string deliveryAddress, string orderNo)
         {
             // Arrange
             var orderToUpdate = new Models.Order { OrderId = 1, CreatedDate = DateTime.Now, CustomerId = 1, DeliveryAddress = "Boulevard", OrderNo = "1" };
 
             _sqlService.Setup(service => service.UpdateOrder(orderToUpdate))
-                .ReturnsAsync(new Models.Order { OrderId = 1, CreatedDate = DateTime.Now, CustomerId = 1, DeliveryAddress = "Updated", OrderNo = "1" });
+                .ReturnsAsync(new Models.Order { OrderId = orderId, CreatedDate = createdDate, CustomerId = customerId, DeliveryAddress = deliveryAddress, OrderNo = orderNo });
 
             // Act
             var actual = await _sut.UpdateOrder(orderToUpdate);
@@ -69,6 +73,15 @@ namespace Order.Microservice.Tests.Unit
             // Assert
             actual?.Should().NotBeNull();
             actual?.DeliveryAddress.Should().Be("Updated");
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            return new List<object[]>
+            {
+                new object[] {1, DateTime.Now, 1, "Updated", "1"},
+                new object[] {1, DateTime.Now, "IdAsString", "Updated", "1" }
+            };
         }
     }
 }
